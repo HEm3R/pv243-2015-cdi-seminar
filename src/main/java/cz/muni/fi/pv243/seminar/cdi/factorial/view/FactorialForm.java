VaclavChalupa/a4m36jee-2014-cdi-seminar

[cdi-seminar] Finish view bean for the FactorialImpl component

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/view/FactorialForm.java
@@ -2,17 +2,27 @@ package cz.ctu.fee.a4m36jee.seminar.cdi.factorial.view;
 
 import java.math.BigInteger;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.Factorial;
+
 /**
  * Backing bean for the factorial form (factorial.xhtml)
  */
-// TODO: turn this class into a named bean with name "factorial"
+@Named("factorial")
+@RequestScoped
 public class FactorialForm {
 
     private Long input;
     private BigInteger result;
 
+    @Inject
+    private Factorial factorial;
+
     public void compute() {
-        // TODO: implement using Factorial bean
+        result = factorial.compute(input);
     }
 
     public void reset() {

HEm3R/pv243-2015-cdi-seminar

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/view/FactorialForm.java
@@ -2,17 +2,27 @@ package cz.muni.fi.pv243.seminar.cdi.factorial.view;
 
 import java.math.BigInteger;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cz.muni.fi.pv243.seminar.cdi.factorial.Factorial;
+
 /**
  * Backing bean for the factorial form (factorial.xhtml)
  */
-// TODO: turn this class into a named bean with name "factorial"
+@Named("factorial")
+@RequestScoped
 public class FactorialForm {
 
     private Long input;
     private BigInteger result;
 
+    @Inject
+    private Factorial factorial;
+
     public void compute() {
-        // TODO: implement using Factorial bean
+        result = factorial.compute(input);
     }
 
     public void reset() {