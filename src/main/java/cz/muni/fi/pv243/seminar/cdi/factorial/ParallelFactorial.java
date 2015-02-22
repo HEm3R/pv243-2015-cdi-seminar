VaclavChalupa/a4m36jee-2014-cdi-seminar

[cdi-seminar] Add parallel implementation of Factorial

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/ParallelFactorial.java
@@ -1,0 +1,38 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.factorial;
+
+import java.math.BigInteger;
+import java.util.concurrent.Future;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Implementation of factorial that is capable of computing factorial in two threads by splitting the range into two
+ * parts.
+ *
+ * @author Jozef Hartinger
+ */
+@Parallel
+@ApplicationScoped
+@Named
+public class ParallelFactorial extends FactorialImpl {
+
+    @Inject
+    private MathOperations math;
+
+    @Override
+    public BigInteger compute(long number) {
+        if (number > 2) {
+            long part = number / 2;
+            Future<BigInteger> firstPart = math.multiplySequenceAsync(1, part);
+            Future<BigInteger> secondPart = math.multiplySequenceAsync(part + 1, number);
+            try {
+                return firstPart.get().multiply(secondPart.get());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return super.compute(number);
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/view/FactorialForm.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.Factorial;
+import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.Parallel;
 
 /**
  * Backing bean for the factorial form (factorial.xhtml)
@@ -19,6 +20,7 @@ public class FactorialForm {
     private BigInteger result;
 
     @Inject
+    @Parallel
     private Factorial factorial;
 
     public void compute() {

src/test/java/cz/ctu/fee/a4m36jee/seminar/cdi/FactorialTest.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.Factorial;
+import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.Parallel;
 import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.util.FactorialComputationFinished;
 
 import static org.junit.Assert.assertEquals;
@@ -24,6 +25,7 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(Arquillian.class)
 public class FactorialTest {
 
+    @Parallel
     @Inject
     private Factorial factorial;
 

HEm3R/pv243-2015-cdi-seminar

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/ParallelFactorial.java
@@ -1,0 +1,38 @@
+package cz.muni.fi.pv243.seminar.cdi.factorial;
+
+import java.math.BigInteger;
+import java.util.concurrent.Future;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Implementation of factorial that is capable of computing factorial in two threads by splitting the range into two
+ * parts.
+ *
+ * @author Jozef Hartinger
+ */
+@Parallel
+@ApplicationScoped
+@Named
+public class ParallelFactorial extends FactorialImpl {
+
+    @Inject
+    private MathOperations math;
+
+    @Override
+    public BigInteger compute(long number) {
+        if (number > 2) {
+            long part = number / 2;
+            Future<BigInteger> firstPart = math.multiplySequenceAsync(1, part);
+            Future<BigInteger> secondPart = math.multiplySequenceAsync(part + 1, number);
+            try {
+                return firstPart.get().multiply(secondPart.get());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return super.compute(number);
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/view/FactorialForm.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import cz.muni.fi.pv243.seminar.cdi.factorial.Factorial;
+import cz.muni.fi.pv243.seminar.cdi.factorial.Parallel;
 
 /**
  * Backing bean for the factorial form (factorial.xhtml)
@@ -19,6 +20,7 @@ public class FactorialForm {
     private BigInteger result;
 
     @Inject
+    @Parallel
     private Factorial factorial;
 
     public void compute() {

src/test/java/cz/muni/fi/pv243/FactorialTest.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import cz.muni.fi.pv243.seminar.cdi.factorial.Factorial;
+import cz.muni.fi.pv243.seminar.cdi.factorial.Parallel;
 import cz.muni.fi.pv243.seminar.cdi.factorial.util.FactorialComputationFinished;
 
 import static org.junit.Assert.assertEquals;
@@ -24,6 +25,7 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(Arquillian.class)
 public class FactorialTest {
 
+    @Parallel
     @Inject
     private Factorial factorial;
 